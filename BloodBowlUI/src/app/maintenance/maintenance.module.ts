c46e3c16387c9b5e04b94b368b7d5e4eea030f51
Thomas Langford <citemysource@hotmail.com>
2021-03-11 00:47:19 +0000

    Added basic skill table

--- BloodBowlUI/src/app/maintenance/maintenance.module.ts
+++ BloodBowlUI/src/app/maintenance/maintenance.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MatTableModule } from '@angular/material/table';
 
 import { MaintenanceRoutingModule } from './maintenance-routing.module';
 import { MaintenanceComponent } from './maintenance.component';
-
+import { SkillFormComponent } from './skill-form/skill-form.component';
 
 @NgModule({
-  declarations: [MaintenanceComponent],
+  declarations: [MaintenanceComponent, SkillFormComponent],
   imports: [
     CommonModule,
+    RouterModule,
+    MatTableModule,
     MaintenanceRoutingModule
   ]
 })
---
+++ BloodBowlUI/src/app/maintenance/skill-form/skill-form.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Skill } from 'src/app/models/skill';
+import { SkillService } from 'src/app/services/skill.service';
+
+@Component({
+  templateUrl: './skill-form.component.html',
+  styleUrls: ['./skill-form.component.scss']
+})
+export class SkillFormComponent implements OnInit {
+  skillService: SkillService;
+  skills: Skill[];
+
+  displayedColumns: string[] = ['name', 'icon'];
+  constructor(skillService: SkillService) {
+
+    this.skillService = skillService;
+    this.skills = [{id: 1, name: "test"}];
+  }
+
+  ngOnInit(): void {
+    // this.skillService.getSkills().subscribe({
+    //   next: x => this.skills = x,
+    //   error: err => console.log(err),
+    //   complete: () => console.log("done")
+    // });
+  }
+
+}
